6fcb6ff5e3d7794fe327fa71cc37892d6b25218a
Alex Barker <alexbark@gmail.com>
2021-12-15 22:51:53 +1100

    add resource

---
+++ TradeUp.Core/Requests/AddResourceRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace TradeUp.Core.Requests
+{
+    public class AddResourceRequest
+    {
+        public string Name { get; set; }
+        public int AvailableAmount { get; set; }
+        public double Price { get; set; }
+    }
+}
--- TradeUp.Core/Services/ResourceService.cs
+++ TradeUp.Core/Services/ResourceService.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
+using TradeUp.Core.Requests;
 using TradeUp.Domain;
 
 namespace TradeUp.Core.Services
@@ -7,6 +9,7 @@ namespace TradeUp.Core.Services
     public interface IResourceService
     {
         public Task<IList<Resource>> GetResources();
+        public Task<ActionResult> AddResource(AddResourceRequest request);
     }
 
     public class ResourceService : IResourceService
@@ -25,6 +28,21 @@ namespace TradeUp.Core.Services
                     .OrderBy(i => i.Name)
                     .ToListAsync();
             }
+        }
+
+        public async Task<ActionResult> AddResource(AddResourceRequest request)
+        {
+            using var context = new TradeUpContext();
+            await context.Resources.AddAsync(new Resource()
+            {
+                Name = request.Name,
+                CountAvailable = request.AvailableAmount,
+                Price = request.Price
+            });
+
+            await context.SaveChangesAsync();
+
+            return new OkResult();
         }
     }
 }
