17d2ce3088444991e71f0c16daa283b5bd80c304
Alex Barker <alexbark@gmail.com>
2021-12-16 21:38:20 +1100

    add process to service

--- TradeUp.Core/Services/ResourceContributorService.cs
+++ TradeUp.Core/Services/ResourceContributorService.cs
@@ -8,6 +8,8 @@ namespace TradeUp.Core.Services
     public interface IResourceContributorService
     {
         Task<ActionResult> AddResourceContributor(AddResourceContributorRequest request);
+        Task<IList<ResourceContributor>> GetResourceContributors();
+        Task ProcessContribution(ResourceContributor toProcess);
     }
     public class ResourceContributorService : IResourceContributorService
     {
@@ -27,7 +29,7 @@ namespace TradeUp.Core.Services
                 var contributor = await context.Contributors
                     .Where(x => x.Id == request.ContributorId)
                     .FirstOrDefaultAsync();
-
+                 
                 if (contributor == null)
                 {
                     return new NotFoundResult();
@@ -46,6 +48,33 @@ namespace TradeUp.Core.Services
                 return new OkResult();
 
             }
+        }
+
+        public async Task<IList<ResourceContributor>> GetResourceContributors()
+        {
+            using var context = new TradeUpContext();
+
+            return await context.ResourceContributors.ToListAsync();
+        }
+
+        public async Task ProcessContribution(ResourceContributor toProcess)
+        {
+            using var context = new TradeUpContext();
+
+            var resourceToModify = await context.Resources.Where(x => x.Id == toProcess.Resource.Id).FirstOrDefaultAsync();
+            var contributor = await context.Contributors.Where(x => x.Id == toProcess.Contributor.Id).FirstOrDefaultAsync();
+
+            if(contributor == null || resourceToModify == null)
+            {
+                throw new ArgumentException("Invalid Contributor or Resource passed in to ProcessContribution");
+            }
+
+            Random rand = new Random();
+            var resourcesToAdd = rand.Next(toProcess.MinContributionRange, toProcess.MaxContributionRange) * contributor.ContributionFactor;
+
+            resourceToModify.CountAvailable += resourcesToAdd;
+
+            await context.SaveChangesAsync();
         }
     }
 }
